1df2954747ef7fa455ce462db9871ffe059b977d
David Tolnay <dtolnay@gmail.com>
2020-08-05 01:12:58 -0700

    Use lowercase canonical capitalization for variants of sort flag

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@
 //! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logoColor=white&logo=data:image/svg+xml;base64,PHN2ZyByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDUxMiA1MTIiPjxwYXRoIGZpbGw9IiNmNWY1ZjUiIGQ9Ik00ODguNiAyNTAuMkwzOTIgMjE0VjEwNS41YzAtMTUtOS4zLTI4LjQtMjMuNC0zMy43bC0xMDAtMzcuNWMtOC4xLTMuMS0xNy4xLTMuMS0yNS4zIDBsLTEwMCAzNy41Yy0xNC4xIDUuMy0yMy40IDE4LjctMjMuNCAzMy43VjIxNGwtOTYuNiAzNi4yQzkuMyAyNTUuNSAwIDI2OC45IDAgMjgzLjlWMzk0YzAgMTMuNiA3LjcgMjYuMSAxOS45IDMyLjJsMTAwIDUwYzEwLjEgNS4xIDIyLjEgNS4xIDMyLjIgMGwxMDMuOS01MiAxMDMuOSA1MmMxMC4xIDUuMSAyMi4xIDUuMSAzMi4yIDBsMTAwLTUwYzEyLjItNi4xIDE5LjktMTguNiAxOS45LTMyLjJWMjgzLjljMC0xNS05LjMtMjguNC0yMy40LTMzLjd6TTM1OCAyMTQuOGwtODUgMzEuOXYtNjguMmw4NS0zN3Y3My4zek0xNTQgMTA0LjFsMTAyLTM4LjIgMTAyIDM4LjJ2LjZsLTEwMiA0MS40LTEwMi00MS40di0uNnptODQgMjkxLjFsLTg1IDQyLjV2LTc5LjFsODUtMzguOHY3NS40em0wLTExMmwtMTAyIDQxLjQtMTAyLTQxLjR2LS42bDEwMi0zOC4yIDEwMiAzOC4ydi42em0yNDAgMTEybC04NSA0Mi41di03OS4xbDg1LTM4Ljh2NzUuNHptMC0xMTJsLTEwMiA0MS40LTEwMi00MS40di0uNmwxMDItMzguMiAxMDIgMzguMnYuNnoiPjwvcGF0aD48L3N2Zz4K
 
 use atty::Stream::Stderr;
-use clap::arg_enum;
 use rustc_demangle::demangle;
 use std::collections::HashMap as Map;
 use std::env;
@@ -14,6 +13,7 @@ use std::fs::{self, File};
 use std::io::{self, ErrorKind, Read, Write};
 use std::path::{Path, PathBuf};
 use std::process::{self, Command, Stdio};
+use std::str::FromStr;
 use structopt::StructOpt;
 use tempdir::TempDir;
 
@@ -171,11 +171,29 @@ impl Instantiations {
     }
 }
 
-arg_enum! {
-    #[derive(Debug)]
-    enum SortOrder {
-        Lines,
-        Copies,
+#[derive(Debug)]
+enum SortOrder {
+    Lines,
+    Copies,
+}
+
+impl SortOrder {
+    fn variants() -> [&'static str; 2] {
+        ["lines", "copies"]
+    }
+}
+
+impl FromStr for SortOrder {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.eq_ignore_ascii_case("lines") {
+            Ok(SortOrder::Lines)
+        } else if s.eq_ignore_ascii_case("copies") {
+            Ok(SortOrder::Copies)
+        } else {
+            Err(format!("valid values: {}", Self::variants().join(", ")))
+        }
     }
 }
 
