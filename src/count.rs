d22ab951d2949577449bc80da33d915a29db9f7f
David Tolnay <dtolnay@gmail.com>
2022-11-21 23:45:27 -0800

    Move parsing/counting to module

---
+++ src/count.rs
@@ -0,0 +1,62 @@
+use rustc_demangle::demangle;
+use std::collections::HashMap as Map;
+
+#[derive(Default)]
+pub struct Instantiations {
+    pub copies: usize,
+    pub total_lines: usize,
+}
+
+impl Instantiations {
+    fn record_lines(&mut self, lines: usize) {
+        self.copies += 1;
+        self.total_lines += lines;
+    }
+}
+
+pub fn count_lines(instantiations: &mut Map<String, Instantiations>, ir: &[u8]) {
+    let mut current_function = None;
+    let mut count = 0;
+
+    for line in String::from_utf8_lossy(ir).lines() {
+        if line.starts_with("define ") {
+            current_function = parse_function_name(line);
+        } else if line == "}" {
+            if let Some(name) = current_function.take() {
+                instantiations
+                    .entry(name)
+                    .or_insert_with(Default::default)
+                    .record_lines(count);
+            }
+            count = 0;
+        } else if line.starts_with("  ") && !line.starts_with("   ") {
+            count += 1;
+        }
+    }
+}
+
+fn parse_function_name(line: &str) -> Option<String> {
+    let start = line.find('@')? + 1;
+    let end = line[start..].find('(')?;
+    let mangled = line[start..start + end].trim_matches('"');
+    let mut name = demangle(mangled).to_string();
+    if has_hash(&name) {
+        let len = name.len() - 19;
+        name.truncate(len);
+    }
+    Some(name)
+}
+
+fn has_hash(name: &str) -> bool {
+    let mut bytes = name.bytes().rev();
+    for _ in 0..16 {
+        if !bytes.next().map_or(false, is_ascii_hexdigit) {
+            return false;
+        }
+    }
+    bytes.next() == Some(b'h') && bytes.next() == Some(b':') && bytes.next() == Some(b':')
+}
+
+fn is_ascii_hexdigit(byte: u8) -> bool {
+    (b'0'..=b'9').contains(&byte) || (b'a'..=b'f').contains(&byte)
+}
--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,15 @@
     clippy::unseparated_literal_suffix
 )]
 
+mod count;
 mod opts;
 mod table;
 
+use crate::count::{count_lines, Instantiations};
 use crate::opts::{SortOrder, Subcommand};
 use atty::Stream::Stderr;
 use clap::{CommandFactory, Parser};
 use regex::Regex;
-use rustc_demangle::demangle;
 use std::collections::HashMap as Map;
 use std::env;
 use std::ffi::{OsStr, OsString};
@@ -184,66 +185,6 @@ fn read_llvm_ir_from_paths(
 
     table::print(instantiations, sort_order, function_filter);
     Ok(0)
-}
-
-#[derive(Default)]
-struct Instantiations {
-    copies: usize,
-    total_lines: usize,
-}
-
-impl Instantiations {
-    fn record_lines(&mut self, lines: usize) {
-        self.copies += 1;
-        self.total_lines += lines;
-    }
-}
-
-fn count_lines(instantiations: &mut Map<String, Instantiations>, ir: &[u8]) {
-    let mut current_function = None;
-    let mut count = 0;
-
-    for line in String::from_utf8_lossy(ir).lines() {
-        if line.starts_with("define ") {
-            current_function = parse_function_name(line);
-        } else if line == "}" {
-            if let Some(name) = current_function.take() {
-                instantiations
-                    .entry(name)
-                    .or_insert_with(Default::default)
-                    .record_lines(count);
-            }
-            count = 0;
-        } else if line.starts_with("  ") && !line.starts_with("   ") {
-            count += 1;
-        }
-    }
-}
-
-fn parse_function_name(line: &str) -> Option<String> {
-    let start = line.find('@')? + 1;
-    let end = line[start..].find('(')?;
-    let mangled = line[start..start + end].trim_matches('"');
-    let mut name = demangle(mangled).to_string();
-    if has_hash(&name) {
-        let len = name.len() - 19;
-        name.truncate(len);
-    }
-    Some(name)
-}
-
-fn has_hash(name: &str) -> bool {
-    let mut bytes = name.bytes().rev();
-    for _ in 0..16 {
-        if !bytes.next().map_or(false, is_ascii_hexdigit) {
-            return false;
-        }
-    }
-    bytes.next() == Some(b'h') && bytes.next() == Some(b':') && bytes.next() == Some(b':')
-}
-
-fn is_ascii_hexdigit(byte: u8) -> bool {
-    (b'0'..=b'9').contains(&byte) || (b'a'..=b'f').contains(&byte)
 }
 
 // Based on https://github.com/rsolomo/cargo-check
